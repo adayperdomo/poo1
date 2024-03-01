06dc6211498083c93beb90817bcdb24262c5c6c2
adayperdomo <adayperdomorodriguez@gmail.com>
2024-03-01 12:59:57 +0000

    main resultado

--- calculo.py
+++ calculo.py
@@ -5,14 +5,23 @@ class Calculo:
 
     def suma(self):
         self.numero1 + self.numero2
-
+        print("El resultado de la suma es:", self.numero1 + self.numero2)
+        
     def resta(self):
         self.numero1 - self.numero2
+        print("El resultado de la resta es:", self.numero1 - self.numero2)
 
     def mult(self):
         self.numero1 * self.numero2
+        print("El resultado de la multiplicación es:", self.numero1 * self.numero2)
 
     def divi(self):
         self.numero1 / self.numero2
+        print("El resulto de la división es:", self.numero1 / self.numero2)
 
-    
+if __name__ == "__main__":
+    ejemplo = Calculo(5,5)
+    ejemplo.suma()
+    ejemplo.resta()
+    ejemplo.mult()
+    ejemplo.divi()
