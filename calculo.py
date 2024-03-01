044696b7ca2167cbdb18545e4fdf8c813abf0dc1
adayperdomo <adayperdomorodriguez@gmail.com>
2024-03-01 12:28:34 +0000

    función operación

---
+++ calculo.py
@@ -0,0 +1,18 @@
+class Calculo:
+    def __init__(self, numero1:int, numero2:int):
+        self.numero1 = numero1
+        self.numero2 = numero2
+
+    def suma(self):
+        self.numero1 + self.numero2
+
+    def resta(self):
+        self.numero1 - self.numero2
+
+    def mult(self):
+        self.numero1 * self.numero2
+
+    def divi(self):
+        self.numero1 / self.numero2
+
+    
